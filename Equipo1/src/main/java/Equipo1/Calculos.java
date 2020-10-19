082be818a15f48d6d6ab5728a07093ffd0391276
mariosoler <mario.solerga@elorrieta-errekamari.com>
2020-10-19 20:07:37 +0200

    Crear metodo suma, comentar cuadrado

--- Equipo1/src/main/java/Equipo1/Calculos.java
+++ Equipo1/src/main/java/Equipo1/Calculos.java
@@ -2,10 +2,16 @@ package Equipo1;
 
 public class Calculos {
 	
+	public int suma(int x,int y) {
+		return x+y;
+		
+	}
+	
+	/*
 	public float cuadrado(int a) {
 		
 		return (float) Math.sqrt(a);
-	}
+	}*/
 	
 	
 	
